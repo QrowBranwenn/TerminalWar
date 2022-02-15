a7816b9d4570b17d0d7867f230eea9c46a169611
QrowBranwenn <mchazen922@gmail.com>
2022-02-15 01:24:46 -0500

    colors-v1.3

--- newDeck copy.js
+++ newDeck copy.js
@@ -90,7 +90,7 @@ for (let i = 0; i < 5; i++) {
 }
 
 cl("");
-cl("Your hand:");
+cl("\x1b[3m", "Your hand:");
 cl("");
 for (let i = 1; i < players.player.length + 1; i++) {
   cl(
@@ -101,7 +101,7 @@ for (let i = 1; i < players.player.length + 1; i++) {
 }
 cl("");
 
-rl.setPrompt(`Please select a card to play. `);
+rl.setPrompt("Please select a card to play.");
 rl.prompt();
 rl.on("line", (selection) => {
   gameRound(selection);
@@ -119,134 +119,134 @@ let winner = function (p1, p2, p3, p4) {
     p1[0].rank > p3[0].rank &&
     p1[0].rank > p4[0].rank
   ) {
-    cl("Player has won this round!");
+    cl("\x1b[36m%s\x1b[3m", "Player has won this round!");
     return (playerWins += 1);
   } else if (
     p2[0].rank > p1[0].rank &&
     p2[0].rank > p3[0].rank &&
     p2[0].rank > p4[0].rank
   ) {
-    cl("Cpu-1 has won this round!");
+    cl("\x1b[33m%s\x1b[3m", "Cpu-1 has won this round!");
     return (cpu1Wins += 1);
   } else if (
     p3[0].rank > p1[0].rank &&
     p3[0].rank > p2[0].rank &&
     p3[0].rank > p4[0].rank
   ) {
-    cl("Cpu-2 has won this round!");
+    cl("\x1b[34m%s\x1b[3m", "Cpu-2 has won this round!");
     return (cpu2Wins += 1);
   } else if (
     p4[0].rank > p1[0].rank &&
     p4[0].rank > p2[0].rank &&
     p4[0].rank > p3[0].rank
   ) {
-    cl("Cpu-3 has won this round!");
+    cl("\x1b[35m%s\x1b[3m", "Cpu-3 has won this round!");
     return (cpu3Wins += 1);
   } else if (p1[0].rank === p2[0].rank) {
     if (p1[0].color !== p2[0].color) {
       if (p1[0].color === "Black") {
-        cl("Player has won this round!");
+        cl("\x1b[36m%s\x1b[3m", "Player has won this round!");
         return (playerWins += 1);
       } else if (p2[0].color === "Black") {
-        cl("Cpu-1 has won this round!");
+        cl("\x1b[33m%s\x1b[3m", "Cpu-1 has won this round!");
         return (cpu1Wins += 1);
       }
     } else {
       if (p1[0].suit === "Diamonds" || p1[0].suit === "Spades") {
-        cl("Player has won this round!");
+        cl("\x1b[36m%s\x1b[3m", "Player has won this round!");
         return (playerWins += 1);
       } else {
-        cl("Cpu-1 has won this round!");
+        cl("\x1b[33m%s\x1b[3m", "Cpu-1 has won this round!");
         return (cpu1Wins += 1);
       }
     }
   } else if (p1[0].rank === p3[0].rank) {
     if (p1[0].color !== p3[0].color) {
       if (p1[0].color === "Black") {
-        cl("Player has won this round!");
+        cl("\x1b[36m%s\x1b[3m", "Player has won this round!");
         return (playerWins += 1);
       } else if (p3[0].color === "Black") {
-        cl("Cpu-2 has won this round!");
+        cl("\x1b[34m%s\x1b[3m", "Cpu-2 has won this round!");
         return (cpu2Wins += 1);
       }
     } else {
       if (p1[0].suit === "Diamonds" || p1[0].suit === "Spades") {
-        cl("Player has won this round!");
+        cl("\x1b[36m%s\x1b[3m", "Player has won this round!");
         return (playerWins += 1);
       } else {
-        cl("Cpu-2 has won this round!");
+        cl("\x1b[34m%s\x1b[3m", "Cpu-2 has won this round!");
         return (cpu2Wins += 1);
       }
     }
   } else if (p2[0].rank === p3[0].rank) {
     if (p2[0].color !== p3[0].color) {
       if (p2[0].color === "Black") {
-        cl("Cpu-1 has won this round!");
+        cl("\x1b[33m%s\x1b[3m", "Cpu-1 has won this round!");
         return (cpu1Wins += 1);
       } else if (p3[0].color === "Black") {
-        cl("Cpu-2 has won this round!");
+        cl("\x1b[34m%s\x1b[3m", "Cpu-2 has won this round!");
         return (cpu2Wins += 1);
       }
     } else {
       if (p2[0].suit === "Diamonds" || p2[0].suit === "Spades") {
-        cl("Cpu-1 has won this round!");
+        cl("\x1b[33m%s\x1b[3m", "Cpu-1 has won this round!");
         return (cpu1Wins += 1);
       } else {
-        cl("Cpu-1 has won this round!");
+        cl("\x1b[34m%s\x1b[3m", "Cpu-2 has won this round!");
         return (cpu2Wins += 1);
       }
     }
   } else if (p4[0].rank === p1[0].rank) {
     if (p4[0].color !== p1[0].color) {
       if (p4[0].color === "Black") {
-        cl("Cpu-3 has won this round!");
+        cl("\x1b[35m%s\x1b[3m", "Cpu-3 has won this round!");
         return (cpu3Wins += 1);
       } else if (p1[0].color === "Black") {
-        cl("Player has won this round!");
+        cl("\x1b[36m%s\x1b[3m", "Player has won this round!");
         return (playerWins += 1);
       }
     } else {
       if (p4[0].suit === "Diamonds" || p4[0].suit === "Spades") {
-        cl("Cpu-3 has won this round!");
+        cl("\x1b[35m%s\x1b[3m", "Cpu-3 has won this round!");
         return (cpu3Wins += 1);
       } else {
-        cl("Player has won this round!");
+        cl("\x1b[36m%s\x1b[3m", "Player has won this round!");
         return (playerWins += 1);
       }
     }
   } else if (p4[0].rank === p2[0].rank) {
     if (p4[0].color !== p2[0].color) {
       if (p4[0].color === "Black") {
-        cl("Cpu-3 has won this round!");
+        cl("\x1b[35m%s\x1b[3m", "Cpu-3 has won this round!");
         return (cpu3Wins += 1);
       } else if (p2[0].color === "Black") {
-        cl("Cpu-1 has won this round!");
+        cl("\x1b[33m%s\x1b[3m", "Cpu-1 has won this round!");
         return (cpu1Wins += 1);
       }
     } else {
       if (p4[0].suit === "Diamonds" || p4[0].suit === "Spades") {
-        cl("Cpu-3 has won this round!");
+        cl("\x1b[35m%s\x1b[3m", "Cpu-3 has won this round!");
         return (cpu3Wins += 1);
       } else {
-        cl("Cpu-1 has won this round!");
+        cl("\x1b[33m%s\x1b[3m", "Cpu-1 has won this round!");
         return (cpu1Wins += 1);
       }
     }
   } else if (p4[0].rank === p3[0].rank) {
     if (p4[0].color !== p3[0].color) {
       if (p4[0].color === "Black") {
-        cl("Cpu-3 has won this round!");
+        cl("\x1b[35m%s\x1b[3m", "Cpu-3 has won this round!");
         return (cpu3Wins += 1);
       } else if (p3[0].color === "Black") {
-        cl("Cpu-2 has won this round!");
+        cl("\x1b[34m%s\x1b[3m", "Cpu-2 has won this round!");
         return (cpu2Wins += 1);
       }
     } else {
       if (p4[0].suit === "Diamonds" || p4[0].suit === "Spades") {
-        cl("Cpu-3 has won this round!");
+        cl("\x1b[35m%s\x1b[3m", "Cpu-3 has won this round!");
         return (cpu3Wins += 1);
       } else {
-        cl("Cpu-2 has won this round!");
+        cl("\x1b[34m%s\x1b[3m", "Cpu-2 has won this round!");
         return (cpu2Wins += 1);
       }
     }
@@ -257,6 +257,7 @@ let gameRound = function (number) {
   if (determine > players.player.length || determine < 1) {
     cl("");
     cl(
+      "\x1b[33m\x1b[3m",
       `Invalid number! Please enter a number between 1 and ${players.player.length}.`
     );
     cl("");
@@ -266,13 +267,13 @@ let gameRound = function (number) {
     if (deck.length !== 0) {
       players.player.push(dealCard());
     }
-    cl(`Round: ${roundCount}`);
-    cl("--------------------------------------------------");
+    cl("\x1b[1m", `Round: ${roundCount}`);
+    cl("\x1b[1m", "--------------------------------------------------");
     cl("");
-    cl("You played:");
+    cl("\x1b[36m%s\x1b[3m", "You played:");
     cl(`The ${choice[0].name} of ${choice[0].suit}. Value: ${choice[0].rank}`);
     cl("");
-    cl("--------------------------------------------------");
+    cl("\x1b[1m", "--------------------------------------------------");
     rl.pause();
 
     let cpu1Play = players.cpu1.splice(
@@ -283,12 +284,12 @@ let gameRound = function (number) {
       players.cpu1.push(dealCard());
     }
     cl("");
-    cl("Cpu-1 played:");
+    cl("\x1b[33m%s\x1b[3m", "Cpu-1 played:");
     cl(
       `The ${cpu1Play[0].name} of ${cpu1Play[0].suit}. Value: ${cpu1Play[0].rank}`
     );
     cl("");
-    cl("--------------------------------------------------");
+    cl("\x1b[1m", "--------------------------------------------------");
 
     let cpu2Play = players.cpu2.splice(
       Math.floor(Math.random() * players.cpu2.length),
@@ -298,12 +299,12 @@ let gameRound = function (number) {
       players.cpu2.push(dealCard());
     }
     cl("");
-    cl("Cpu-2 played:");
+    cl("\x1b[34m%s\x1b[3m", "Cpu-2 played:");
     cl(
       `The ${cpu2Play[0].name} of ${cpu2Play[0].suit}. Value: ${cpu2Play[0].rank}`
     );
     cl("");
-    cl("--------------------------------------------------");
+    cl("\x1b[1m", "--------------------------------------------------");
 
     let cpu3Play = players.cpu3.splice(
       Math.floor(Math.random() * players.cpu3.length),
@@ -313,17 +314,17 @@ let gameRound = function (number) {
       players.cpu3.push(dealCard());
     }
     cl("");
-    cl("Cpu-3 played:");
+    cl("\x1b[35m%s\x1b[3m", "Cpu-3 played:");
     cl(
       `The ${cpu3Play[0].name} of ${cpu3Play[0].suit}. Value: ${cpu3Play[0].rank}`
     );
     cl("");
-    cl("--------------------------------------------------");
+    cl("\x1b[1m", "--------------------------------------------------");
     cl("");
     winner(choice, cpu1Play, cpu2Play, cpu3Play);
     roundCount += 1;
     cl("");
-    cl("--------------------------------------------------");
+    cl("\x1b[1m", "--------------------------------------------------");
   }
   if (
     players.player.length === 0 ||
@@ -332,18 +333,22 @@ let gameRound = function (number) {
     players.cpu3.length === 0
   ) {
     cl("");
-    cl("--------------------------------------------------");
+    cl("\x1b[0m\x1b[1m", "--------------------------------------------------");
     cl("");
-    cl("The game is over as all players have run out of cards!");
+    cl(
+      "\x1b[0m\x1b[1m",
+      "The game is over as all players have run out of cards!"
+    );
     cl("");
     cl(finalScore(playerWins, cpu1Wins, cpu2Wins, cpu3Wins));
     cl("");
-    cl("--------------------------------------------------");
+    cl("\x1b[0m\x1b[1m", "--------------------------------------------------");
     cl("");
     rl.close();
   } else {
     cl("");
-    cl("Your hand:");
+    cl("\x1b[1m\x1b[0m", "Your hand:");
+    cl("");
     for (let i = 1; i < players.player.length + 1; i++) {
       cl(
         `${i}) ${players.player[i - 1].name} of ${
@@ -359,41 +364,45 @@ let gameRound = function (number) {
 
 let finalScore = function (p1, p2, p3, p4) {
   if (p1 > p2 && p1 > p3 && p1 > p4) {
-    return `Player has the most wins with ${p1} in total!`;
+    return "\x1b[1m", `Player has the most wins with ${p1} in total!`;
   } else if (p2 > p1 && p2 > p3 && p2 > p4) {
-    return `Cpu-1 has the most wins with ${p2} in total!`;
+    return "\x1b[1m", `Cpu-1 has the most wins with ${p2} in total!`;
   } else if (p3 > p1 && p3 > p2 && p3 > p4) {
-    return `Cpu-2 has the most wins with ${p3} in total!`;
+    return "\x1b[1m", `Cpu-2 has the most wins with ${p3} in total!`;
   } else if (p4 > p1 && p4 > p2 && p4 > p3) {
-    return `Cpu-3 has the most wins with ${p4} in total!`;
+    return "\x1b[1m", `Cpu-3 has the most wins with ${p4} in total!`;
   } else {
     if (p1 === 0) {
-      return `No one's a winner!`;
+      return "\x1b[1m", `No one's a winner!`;
     } else if (p1 === p3) {
-      return `Player and Cpu-2 have tied with ${p1} wins each!`;
+      return "\x1b[1m", `Player and Cpu-2 have tied with ${p1} wins each!`;
     } else if (p2 === p3) {
-      return `Both Cpu-1 and Cpu-2 have tied with ${p2} wins each!`;
+      return "\x1b[1m", `Both Cpu-1 and Cpu-2 have tied with ${p2} wins each!`;
     } else if (p1 === p4) {
-      return `Player and Cpu-3 have tied with ${p1} wins each!`;
+      return "\x1b[1m", `Player and Cpu-3 have tied with ${p1} wins each!`;
     } else if (p2 === p4) {
-      return `Both Cpu-1 and Cpu-3 have tied with ${p2} wins each!`;
+      return "\x1b[1m", `Both Cpu-1 and Cpu-3 have tied with ${p2} wins each!`;
     } else if (p3 === p4) {
-      return `Both Cpu-2 and Cpu-3 have tied with ${p3} wins each!`;
+      return "\x1b[1m", `Both Cpu-2 and Cpu-3 have tied with ${p3} wins each!`;
     } else if (p1 === p2) {
-      return `Player and Cpu-1 have tied with ${p1} wins each!`;
+      return "\x1b[1m", `Player and Cpu-1 have tied with ${p1} wins each!`;
     } else {
-      return `Somehow, all players tied!`;
+      return "\x1b[1m", `Somehow, all players tied!`;
     }
   }
 };
 
 rl.on("SIGINT", () => {
-  rl.question("Exit (y or n)? ", (input) => {
+  rl.question("\x1b[1m\x1b[0m Exit (y or n)? ", (input) => {
     if (input.match(/^y(es)?$/i)) {
       cl("");
-      cl("--------------------------------------------------");
+      cl(
+        "\x1b[1m\x1b[0m",
+        "--------------------------------------------------"
+      );
       cl("");
       cl(
+        "\x1b[1m\x1b[0m",
         `Game aborted early. ${finalScore(
           playerWins,
           cpu1Wins,
@@ -402,7 +411,10 @@ rl.on("SIGINT", () => {
         )}`
       );
       cl("");
-      cl("--------------------------------------------------");
+      cl(
+        "\x1b[1m\x1b[0m",
+        "--------------------------------------------------"
+      );
       cl("");
       rl.pause();
     }
