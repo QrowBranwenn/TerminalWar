f71d38468893f384c7b1e3f817f21de0dc877981
QrowBranwenn <mchazen922@gmail.com>
2022-02-15 00:39:06 -0500

    cleaner-v1.2

--- newDeck copy.js
+++ newDeck copy.js
@@ -2,15 +2,15 @@
   Hello! Welcome to my 100% JavaScript card game!
 
   The rules are simple:
-    1. Select a card from your hand to play by inputting a number (1-7) that corresponds to the ard you wish to play.
+    1. Select a card from your hand to play by inputting a number (1-5) that corresponds to the ard you wish to play.
     2. Highest card wins.
     3. If there is a tie, Black trumps Red.
     4. If there is still a tie, Spades beat Clubs and Diamonds beat Hearts.
-    5. Finally, if you wish to end the game early, simply press control+c. (You will still receive a winner).
+    5. Finally, there are 13 rounds in total. However, if you wish to end the game early, simply press control+c. (You will still receive a winner).
    
   Whichever player wins the most rounds, wins the game!
 
-  *Note that if you input something other than a number 1-7 (or nothing at all), the game will automatically choose the first card in your hand.
+  *Note that if you input something other than a number (or nothing at all), the game will automatically choose the first card in your hand.
 */
 
 const cl = console.log;
@@ -79,16 +79,27 @@ let players = {
   player: [],
   cpu1: [],
   cpu2: [],
+  cpu3: [],
 };
 
-for (let i = 0; i < 7; i++) {
+for (let i = 0; i < 5; i++) {
   players.player.push(dealCard());
   players.cpu1.push(dealCard());
   players.cpu2.push(dealCard());
+  players.cpu3.push(dealCard());
 }
 
+cl("");
 cl("Your hand:");
-ct(players.player);
+cl("");
+for (let i = 1; i < players.player.length + 1; i++) {
+  cl(
+    `${i}) ${players.player[i - 1].name} of ${
+      players.player[i - 1].suit
+    }. Value: ${players.player[i - 1].rank}`
+  );
+}
+cl("");
 
 rl.setPrompt(`Please select a card to play. `);
 rl.prompt();
@@ -99,17 +110,38 @@ rl.on("line", (selection) => {
 let playerWins = 0;
 let cpu1Wins = 0;
 let cpu2Wins = 0;
+let cpu3Wins = 0;
+let roundCount = 1;
 
-let winner = function (p1, p2, p3) {
-  if (p1[0].rank > p2[0].rank && p1[0].rank > p3[0].rank) {
+let winner = function (p1, p2, p3, p4) {
+  if (
+    p1[0].rank > p2[0].rank &&
+    p1[0].rank > p3[0].rank &&
+    p1[0].rank > p4[0].rank
+  ) {
     cl("Player has won this round!");
     return (playerWins += 1);
-  } else if (p2[0].rank > p1[0].rank && p2[0].rank > p3[0].rank) {
+  } else if (
+    p2[0].rank > p1[0].rank &&
+    p2[0].rank > p3[0].rank &&
+    p2[0].rank > p4[0].rank
+  ) {
     cl("Cpu-1 has won this round!");
     return (cpu1Wins += 1);
-  } else if (p3[0].rank > p1[0].rank && p3[0].rank > p2[0].rank) {
+  } else if (
+    p3[0].rank > p1[0].rank &&
+    p3[0].rank > p2[0].rank &&
+    p3[0].rank > p4[0].rank
+  ) {
     cl("Cpu-2 has won this round!");
     return (cpu2Wins += 1);
+  } else if (
+    p4[0].rank > p1[0].rank &&
+    p4[0].rank > p2[0].rank &&
+    p4[0].rank > p3[0].rank
+  ) {
+    cl("Cpu-3 has won this round!");
+    return (cpu3Wins += 1);
   } else if (p1[0].rank === p2[0].rank) {
     if (p1[0].color !== p2[0].color) {
       if (p1[0].color === "Black") {
@@ -164,21 +196,82 @@ let winner = function (p1, p2, p3) {
         return (cpu2Wins += 1);
       }
     }
+  } else if (p4[0].rank === p1[0].rank) {
+    if (p4[0].color !== p1[0].color) {
+      if (p4[0].color === "Black") {
+        cl("Cpu-3 has won this round!");
+        return (cpu3Wins += 1);
+      } else if (p1[0].color === "Black") {
+        cl("Player has won this round!");
+        return (playerWins += 1);
+      }
+    } else {
+      if (p4[0].suit === "Diamonds" || p4[0].suit === "Spades") {
+        cl("Cpu-3 has won this round!");
+        return (cpu3Wins += 1);
+      } else {
+        cl("Player has won this round!");
+        return (playerWins += 1);
+      }
+    }
+  } else if (p4[0].rank === p2[0].rank) {
+    if (p4[0].color !== p2[0].color) {
+      if (p4[0].color === "Black") {
+        cl("Cpu-3 has won this round!");
+        return (cpu3Wins += 1);
+      } else if (p2[0].color === "Black") {
+        cl("Cpu-1 has won this round!");
+        return (cpu1Wins += 1);
+      }
+    } else {
+      if (p4[0].suit === "Diamonds" || p4[0].suit === "Spades") {
+        cl("Cpu-3 has won this round!");
+        return (cpu3Wins += 1);
+      } else {
+        cl("Cpu-1 has won this round!");
+        return (cpu1Wins += 1);
+      }
+    }
+  } else if (p4[0].rank === p3[0].rank) {
+    if (p4[0].color !== p3[0].color) {
+      if (p4[0].color === "Black") {
+        cl("Cpu-3 has won this round!");
+        return (cpu3Wins += 1);
+      } else if (p3[0].color === "Black") {
+        cl("Cpu-2 has won this round!");
+        return (cpu2Wins += 1);
+      }
+    } else {
+      if (p4[0].suit === "Diamonds" || p4[0].suit === "Spades") {
+        cl("Cpu-3 has won this round!");
+        return (cpu3Wins += 1);
+      } else {
+        cl("Cpu-2 has won this round!");
+        return (cpu2Wins += 1);
+      }
+    }
   }
 };
-
 let gameRound = function (number) {
   let determine = parseInt(number, 10);
-  if (determine > 7 || determine < 1) {
-    cl("Invalid number! Please enter a number between 1 and 7.");
+  if (determine > players.player.length || determine < 1) {
+    cl("");
+    cl(
+      `Invalid number! Please enter a number between 1 and ${players.player.length}.`
+    );
+    cl("");
   } else {
+    console.clear();
     let choice = players.player.splice(determine - 1, 1);
     if (deck.length !== 0) {
       players.player.push(dealCard());
     }
+    cl(`Round: ${roundCount}`);
     cl("--------------------------------------------------");
+    cl("");
     cl("You played:");
-    ct(choice);
+    cl(`The ${choice[0].name} of ${choice[0].suit}. Value: ${choice[0].rank}`);
+    cl("");
     cl("--------------------------------------------------");
     rl.pause();
 
@@ -189,8 +282,12 @@ let gameRound = function (number) {
     if (deck.length !== 0) {
       players.cpu1.push(dealCard());
     }
+    cl("");
     cl("Cpu-1 played:");
-    ct(cpu1Play);
+    cl(
+      `The ${cpu1Play[0].name} of ${cpu1Play[0].suit}. Value: ${cpu1Play[0].rank}`
+    );
+    cl("");
     cl("--------------------------------------------------");
 
     let cpu2Play = players.cpu2.splice(
@@ -200,41 +297,92 @@ let gameRound = function (number) {
     if (deck.length !== 0) {
       players.cpu2.push(dealCard());
     }
+    cl("");
     cl("Cpu-2 played:");
-    ct(cpu2Play);
+    cl(
+      `The ${cpu2Play[0].name} of ${cpu2Play[0].suit}. Value: ${cpu2Play[0].rank}`
+    );
+    cl("");
     cl("--------------------------------------------------");
 
-    winner(choice, cpu1Play, cpu2Play);
-
+    let cpu3Play = players.cpu3.splice(
+      Math.floor(Math.random() * players.cpu3.length),
+      1
+    );
+    if (deck.length !== 0) {
+      players.cpu3.push(dealCard());
+    }
+    cl("");
+    cl("Cpu-3 played:");
+    cl(
+      `The ${cpu3Play[0].name} of ${cpu3Play[0].suit}. Value: ${cpu3Play[0].rank}`
+    );
+    cl("");
     cl("--------------------------------------------------");
-    cl("Your hand:");
-    ct(players.player);
+    cl("");
+    winner(choice, cpu1Play, cpu2Play, cpu3Play);
+    roundCount += 1;
+    cl("");
+    cl("--------------------------------------------------");
   }
-  if (players.cpu1.length === 0) {
-    cl("The game is over as 1 or more players have run out of cards!");
-    cl(finalScore(playerWins, cpu1Wins, cpu2Wins));
+  if (
+    players.player.length === 0 ||
+    players.cpu1.length === 0 ||
+    players.cpu2.length === 0 ||
+    players.cpu3.length === 0
+  ) {
+    cl("");
+    cl("--------------------------------------------------");
+    cl("");
+    cl("The game is over as all players have run out of cards!");
+    cl("");
+    cl(finalScore(playerWins, cpu1Wins, cpu2Wins, cpu3Wins));
+    cl("");
+    cl("--------------------------------------------------");
+    cl("");
     rl.close();
   } else {
+    cl("");
+    cl("Your hand:");
+    for (let i = 1; i < players.player.length + 1; i++) {
+      cl(
+        `${i}) ${players.player[i - 1].name} of ${
+          players.player[i - 1].suit
+        }. Value: ${players.player[i - 1].rank}`
+      );
+    }
+    cl("");
+    rl.prompt();
     rl.resume();
   }
 };
 
-let finalScore = function (p1, p2, p3) {
-  if (p1 > p2 && p1 > p3) {
+let finalScore = function (p1, p2, p3, p4) {
+  if (p1 > p2 && p1 > p3 && p1 > p4) {
     return `Player has the most wins with ${p1} in total!`;
-  } else if (p2 > p1 && p2 > p3) {
+  } else if (p2 > p1 && p2 > p3 && p2 > p4) {
     return `Cpu-1 has the most wins with ${p2} in total!`;
-  } else if (p3 > p1 && p3 > p2) {
+  } else if (p3 > p1 && p3 > p2 && p3 > p4) {
     return `Cpu-2 has the most wins with ${p3} in total!`;
+  } else if (p4 > p1 && p4 > p2 && p4 > p3) {
+    return `Cpu-3 has the most wins with ${p4} in total!`;
   } else {
-    if (p1 === p2) {
-      return `Player and Cpu-1 have tied with ${p1} wins each!`;
+    if (p1 === 0) {
+      return `No one's a winner!`;
     } else if (p1 === p3) {
       return `Player and Cpu-2 have tied with ${p1} wins each!`;
     } else if (p2 === p3) {
       return `Both Cpu-1 and Cpu-2 have tied with ${p2} wins each!`;
+    } else if (p1 === p4) {
+      return `Player and Cpu-3 have tied with ${p1} wins each!`;
+    } else if (p2 === p4) {
+      return `Both Cpu-1 and Cpu-3 have tied with ${p2} wins each!`;
+    } else if (p3 === p4) {
+      return `Both Cpu-2 and Cpu-3 have tied with ${p3} wins each!`;
+    } else if (p1 === p2) {
+      return `Player and Cpu-1 have tied with ${p1} wins each!`;
     } else {
-      return `All players have tied with ${p1} wins each!`;
+      return `Somehow, all players tied!`;
     }
   }
 };
@@ -242,7 +390,20 @@ let finalScore = function (p1, p2, p3) {
 rl.on("SIGINT", () => {
   rl.question("Exit (y or n)? ", (input) => {
     if (input.match(/^y(es)?$/i)) {
-      cl(`Game aborted early. ${finalScore(playerWins, cpu1Wins, cpu2Wins)}`);
+      cl("");
+      cl("--------------------------------------------------");
+      cl("");
+      cl(
+        `Game aborted early. ${finalScore(
+          playerWins,
+          cpu1Wins,
+          cpu2Wins,
+          cpu3Wins
+        )}`
+      );
+      cl("");
+      cl("--------------------------------------------------");
+      cl("");
       rl.pause();
     }
   });
